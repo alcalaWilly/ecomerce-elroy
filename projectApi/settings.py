5150e7331b2a7b155094625536456de86ca1c039
alcalaWilly <willykenneth.alcala@gmail.com>
2025-12-03 13:37:46 -0500

    fix: correccio requirements

--- projectApi/settings.py
+++ projectApi/settings.py
@@ -243,7 +243,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'withenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
